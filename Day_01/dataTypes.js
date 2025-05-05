5ee7f090050467e745817f89b05bfaa1bade36a8
DarshikaNemani <dk5673@srmist.edu.in>
2025-05-05 16:50:16 +0530

    Data Types implementation

---
+++ Day_01/dataTypes.js
@@ -0,0 +1,5 @@
+var a = "Hello World!";
+var b = 55;
+var c = true;
+var d = "55";
+console.log(a + b + c + d);
---
+++ Day_01/dataTypes.ts
@@ -0,0 +1,6 @@
+let a: string = "Hello World!"
+let b: number = 55;
+let c: boolean = true;
+let d: any = "55";
+
+console.log(a + b + c + d);
