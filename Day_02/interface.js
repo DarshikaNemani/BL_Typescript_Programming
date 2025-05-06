9c5fdf7c482d05d0a658936b14c575e0f7bd8a25
DarshikaNemani <dk5673@srmist.edu.in>
2025-05-06 11:18:29 +0530

    Interfaces implementation

---
+++ Day_02/interface.js
@@ -0,0 +1,7 @@
+var ABCD = {
+    a: "A",
+    b: "B",
+    c: 5,
+    d: 10
+};
+console.log(ABCD);
---
+++ Day_02/interface.ts
@@ -0,0 +1,15 @@
+interface abcd {
+    a: string,
+    b: string,
+    c: number,
+    d: number
+}
+
+const ABCD: abcd = {
+    a: "A",
+    b: "B",
+    c: 5,
+    d: 10
+}
+
+console.log(ABCD)
