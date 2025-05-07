f3cc0953bab647a5347f639d01c34fd15e86629f
DarshikaNemani <dk5673@srmist.edu.in>
2025-05-07 15:16:28 +0530

    Car data in constructor using parameter property

---
+++ Day_03/paraCar.js
@@ -0,0 +1,8 @@
+var Car = /** @class */ (function () {
+    function Car(brand, year) {
+        this.brand = brand;
+        this.year = year;
+    }
+    return Car;
+}());
+var car = new Car("ABC", 123);
---
+++ Day_03/paraCar.ts
@@ -0,0 +1,8 @@
+class Car {
+    constructor(
+        public brand: string,
+        public year: number
+    ) { }
+}
+
+const car= new Car("ABC", 123);
