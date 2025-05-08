a9b919ad36a5028738b7c262f16dcf4aff3735f5
DarshikaNemani <dk5673@srmist.edu.in>
2025-05-08 17:35:57 +0530

    Decorator implementation

---
+++ Day_04/deco.ts
@@ -0,0 +1,10 @@
+function A(constructor:Function){
+    console.log("Class B is going to be called",constructor.name);
+}
+@A
+class B{
+   constructor(){
+    console.log("Hello class B called");
+   }
+}
+const obj=new B();
